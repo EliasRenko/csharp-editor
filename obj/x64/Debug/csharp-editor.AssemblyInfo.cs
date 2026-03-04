8df5a4e2e3ca8e12714f89e01ac48fbe25ee0659
Fedorenko Elias <FedorenkoElias@yandex.ru>
2026-03-04 16:55:05 +0200

    Update assembly informational version and regenerate binaries

--- obj/x64/Debug/csharp-editor.AssemblyInfo.cs
+++ obj/x64/Debug/csharp-editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb59eec0ab9f8833ade263b503549d5ff0b72d6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c730bfbe020a8601e75d2861da6adb6b329e0b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
